4afdb2b95deb04325c5e1221c9517c84a9509a41
nadegemokdad <55798672+nadegemokdad@users.noreply.github.com>
2019-11-14 11:50:53 +0100

    maj_1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Table de multiplication</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <img src="" alt="">
+
+        </div>
+        <div class="row justify-content-center">
+
+        </div>
+        <div class="row justify-content-center">
+            <a type="button" class="btn btn-primary col-6" href="tab_multiplication.php">Tables de Multiplication</a>
+            <a type="button" class="btn btn-success col-6" href="tab_revision.php"> Révision</a>
+        </div>
+</body>
+
+</html>
---
+++ tab_multiplication.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Table de multiplication</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <img src="" alt="">
+
+        </div>
+        <div class="row justify-content-center">
+            <h1> Notre slogan </h1>
+        </div>
+        <div class="row justify-content-center">
+            <button class="btn btn-info">Retour</button>
+        </div>
+        <div class="row">
+            <label for="table"> Choisissez une table de multiplication:</label>
+
+            <form action="tab_multiplication.php" method="post">
+                <select name="table" id="table">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                    <option value="13">13</option>
+                    <option value="14">14</option>
+                    <option value="15">15</option>
+                </select>
+                <input type="submit">
+            </form>
+            <div class="row">
+                <?php
+                //test de la variable post
+                if (isset($_POST["table"])) { // verifie la presence de la variable "post[table]
+                    $table = $_POST["table"];
+                    $result;
+    
+                    for ($i = 1; $i < 16; $i++) {// si variable présente, alors il execute for et affiche le resultat
+                        $result = $table * $i;
+    
+                        echo  $table . "x" . $i . " = " . $result . "<br>";
+                    }  
+             
+                }
+                ?>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
